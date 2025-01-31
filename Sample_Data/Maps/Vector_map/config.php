61ada3035c8ca2a6efa49b6004df01aaa1b5a065
VanicekTomas <tm.vanicek@gmail.com>
2025-01-31 09:39:07 +0100

    Doplnění chybějících souborů

--- Sample_Data/Maps/Vector_map/config.php
+++ Sample_Data/Maps/Vector_map/config.php
@@ -8,6 +8,8 @@ $logger = 'xml';  // mysql OR xml cokoliv jiného = demo mod
 $debug = 0; // 0=nezobrazí se debugovací patička ani se do ní nic nevypisuje; 1=zobrazeno a vypisuje se
 
 // INSERT GOOGLE MAPS API KEY
-define('GOOGLE_MAPS_KEY', 'AIzaSyBaDNiLG4-HfJOk9gqnVcPItesj7lehXdY')
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+define('GOOGLE_MAPS_KEY', $_ENV['GOOGLE_MAPS_KEY']);
 
 ?>
